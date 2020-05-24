6fe078975530f97f8def1e52b71c49af09b3c93a
Ryro3753 <20161701053@stu.khas.edu.tr>
2020-05-24 23:13:33 +0300

    WeekDays improvements

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import spacy
 
+nlp = spacy.load("en_core_web_sm")
 months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
 daysText = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 
@@ -9,6 +11,8 @@ daysText = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth',
 
 daysNumber = ['1st','2nd','3rd','4th','5th','6th','7th','8th','9th','10th','11th','12th','13th','14th','15th',
 '16th','17th','18th','19th','20th','21st','22nd','23rd','24th','25th','26th','27th','28th','29th','30th','31st']
+
+weekDays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
 
 past = ['quarter past', 'half past', 'quarter to']
 
@@ -29,12 +33,37 @@ def getMonthAndDate(s):
 
     return (month, day)
 
-for i in getMonthAndDate(text.split()):
-    print(i)
+
 
 
 def getMonthIndex(m):
     return(months.index(m)+1)
 
 
-print(getMonthIndex('December'))
+
+
+
+
+
+
+def ExtractWeekDay(dateList):
+    day = []
+    for date in dateList:
+        if date in weekDays:
+            day.append(weekDays.index(date))
+    return day
+
+def ExtractHour():
+    x = 0
+
+def ExtractMin():
+    x = 0
+
+def ExtractMonth():
+    x = 0
+
+def ExtractDay():
+    x = 0
+
+tt = ['Sunday','Monday']
+print(ExtractWeekDay(tt))
--- testSp.py
+++ testSp.py
@@ -1,6 +1,9 @@
 import spacy
 nlp = spacy.load("en_core_web_sm")
-doc = nlp(u"At 14:15 on eleventh day of January")
+doc = nlp(u"Every even day at January and February between 10th and 20th at 5 past 10")
+doc2 = nlp(u"At 14:45 in January twenty second")
+doc3 = nlp(u"Every Sunday in May, By the way Holland")
+
 
 print("Noun phrases:", [chunk.text for chunk in doc.noun_chunks])
 print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
@@ -8,4 +11,21 @@ print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
 for entity in doc.ents:
     print(entity.text, entity.label_)
 
-print(doc.ents)
+
+
+
+print("Noun phrases:", [chunk.text for chunk in doc2.noun_chunks])
+for entity in doc2.ents:
+    print(entity.text, entity.label_)
+
+print("Noun phrases:", [chunk.text for chunk in doc3.noun_chunks])
+for entity in doc3.ents:
+    print(entity.text, entity.label_)
+
+for entity in doc3.ents:
+    if(entity.label_ == 'DATE' or entity.label_ == 'TIME'):
+        print(entity.text)
+
+
+
+
